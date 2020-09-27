018c3f7e122762e3aa02567e8ec4675e041edf43
Suikasora <S_Aihui@outlook.com>
2020-09-27 23:18:26 +0800

    auto commit

--- java实验1-9/2/Circle.java
+++ java实验1-9/2/Circle.java
@@ -2,11 +2,31 @@ import java.util.Scanner;
 public class Circle{
 	public static void main(String[] args){
 		Scanner s = new Scanner(System.in);
-		int n;
+		int i,j,k,m,n;
 		n = Integer.valueOf(s.next());
-		int a[n];
-		for (int i = 0; i < n; i++) {
-			a[i] = i;
+		int[] a = new int[n];
+		for (i = 0; i < n; i++) {
+			a[i] = i+1;
 		}
+		i = 0;k = 0;m = 0;
+		while( m < n-1 ){
+			if ( a[i] != 0 ) {
+				k++;
+			}
+			if ( k == 3 ) {
+				a[i] = 0;
+				k = 0;
+				m ++;
+			}
+			i++;
+			if (i == n) {
+				i = 0;
+			}
+		}
+		i = 0;
+		while( a[i] == 0 ){
+			i++;
+		}
+		System.out.println(a[i]);
 	}
 }
